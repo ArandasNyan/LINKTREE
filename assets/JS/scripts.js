88f7156da82e783e646d547b2885e853d361dc37
Arandas <99224016+ArandasNyan@users.noreply.github.com>
2022-12-16 03:18:24 -0300

    putting time in title

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -5,3 +5,18 @@ toggle.onclick = function() {
   toggle.classList.toggle('Dark');
   body.classList.toggle('Dark')
 }
+
+/* putting time in title */
+const Title = document.querySelector("title")
+
+const clock = setInterval(function time() {
+  let dateToday = new Date()
+  let hour = dateToday.getHours()
+  let minute = dateToday.getMinutes()
+
+
+  let timeInHour = hour >= 0 && hour <= 9 ? '0' + hour : hour
+  let timeInMinute = minute >= 0 && minute <= 9 ? '0' + minute : minute
+
+  Title.textContent = `Nyan Dev's - ${timeInHour}:${timeInMinute}`
+})
